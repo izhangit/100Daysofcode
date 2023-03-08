e317a2e4fe059076c29f9c52db5da8f1b42a81d9
IzhanAbdullah <108143680+izhangit@users.noreply.github.com>
2023-03-08 20:16:54 +0530

    Delete Day1.py

--- Day1.py
+++
@@ -1,8 +0,0 @@
-print("Izhan Abdullah")
-print("06-03-2023")
-print("""I am Signing up for Replit's 100 days of Python
-I will make sure to spend some time every day coding along, for a minimum of 20 minutes a day.
-I'll be using Replit, an  amazing online IDE so I can do this from from my phone whenever I happen to be. No excuses for not coding from the middle of a field!
-
-
-You can follow my progress at replit.com/@IzhanAbdullah""")
