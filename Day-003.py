df13904f230e3497a6ae483a8c7f31e38b6b89b8
IzhanAbdullah <108143680+izhangit@users.noreply.github.com>
2023-03-28 10:18:54 +0530

    Update Day-003.py

--- Day-003.py
+++ Day-003.py
@@ -4,9 +4,4 @@ yourName = input("Name : ")
 whatYear = input("What year is it? : ")
 
 # Concatenation
-print(yourName, "thinks it is", whatYear)
-
-
-
-
-
+print(yourName, "thinks it is", whatYear)
